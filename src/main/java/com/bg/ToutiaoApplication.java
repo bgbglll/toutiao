f84684e6cb0183ce40f40ff8b8caf5efbf2d9fb4
bg <602566046@qq.com>
2016-06-28 08:50:53 +0800

    Changed

--- src/main/java/com/bg/ToutiaoApplication.java
+++ src/main/java/com/bg/ToutiaoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToutiaoApplication {
 
 	public static void main(String[] args) {
+		//changed
 		SpringApplication.run(ToutiaoApplication.class, args);
 	}
 }
