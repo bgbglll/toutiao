97c3529a7e1514a7b5d59981f822a6d8353ce98b
bg <602566046@qq.com>
2016-07-02 16:11:17 +0800

    integrating mybatis(dao uses interface!!!!) UserDAO

--- src/main/java/com/bg/dao/UserDAO.java
+++ src/main/java/com/bg/dao/UserDAO.java
@@ -1,7 +1,29 @@
 package com.bg.dao;
 
+import com.bg.model.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 /**
  * Created by Administrator on 2016/7/2.
  */
-public class UserDAO {
+public interface UserDAO {
+    String TABLE_NAME = "user";
+    String INSERT_FIELDS = " name, password, salt, head_url ";
+    String SELECT_FIELDS = " id, name, password, salt, head_url";
+
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{name},#{password},#{salt}),#{head_url}"})
+    int addUser(User user);
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
+    User selectById(int id);
+
+    @Update({"update ", TABLE_NAME, " set password=#{password} where id=#{id}"})
+    void updatePassword(User user);
+
+    @Delete({"delete from ", TABLE_NAME, " where id=#{id}"})
+    void deleteById(int id);
 }
