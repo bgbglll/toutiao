3910f3794e3a65ced569f1d539be25fbd387b658
bg <602566046@qq.com>
2016-07-25 14:40:53 +0800

    All page + receive &  send message

--- src/main/java/com/bg/ToutiaoApplication.java
+++ src/main/java/com/bg/ToutiaoApplication.java
@@ -2,10 +2,16 @@ package com.bg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ToutiaoApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ToutiaoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		//changed
